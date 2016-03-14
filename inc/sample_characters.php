5552e247dac0992037d9de56569b4a8a1c931d51
Edward Seibert <taddewberries@gmail.com>
2016-03-14 16:57:18 -0400

    dummy data

---
+++ inc/sample_characters.php
@@ -0,0 +1,23 @@
+<?php 
+$characters = array(
+	0 => array(
+	 	name => "Eddie Dean",
+	 	setting => "Cam/Anarch",
+	 	clan => "Malkavian",
+	 	archetype => "Free Spirit"
+ 		),
+	1 => 	array(
+	 	name => "Mors Ghede",
+	 	setting => "Sabbat",
+	 	clan => "Harbinger of Skulls",
+	 	archetype => "Scientist"
+ 		),
+ 	2 => array(
+	 	name => "Hermes",
+	 	setting => "Cam/Anarch",
+	 	clan => "Brujah",
+	 	archetype => "Troublemaker"
+ 		)
+ 	);
+
+ ?>
