ca61ea2403abbf50c42e4d32431450b80d699dc0
Edward Seibert <taddewberries@gmail.com>
2016-03-14 19:39:09 -0400

    Quote your keys, Ed!

--- inc/sample_characters.php
+++ inc/sample_characters.php
@@ -1,32 +1,32 @@
 <?php 
 $characters = array(
 	0 => array(
-	 	name => "Eddie Dean",
-	 	setting => "Cam/Anarch",
-	 	clan => "Malkavian",
-	 	archetype => "Free Spirit",
-	 	description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione.<br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
+	 	"name" => "Eddie Dean",
+	 	"setting" => "Cam/Anarch",
+	 	"clan" => "Malkavian",
+	 	"archetype" => "Free Spirit",
+	 	"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione.<br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
  		),
 	1 => 	array(
-	 	name => "Mors Ghede",
-	 	setting => "Sabbat",
-	 	clan => "Harbinger of Skulls",
-	 	archetype => "Scientist",
-	 	description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
+	 	"name" => "Mors Ghede",
+	 	"setting" => "Sabbat",
+	 	"clan" => "Harbinger of Skulls",
+	 	"archetype" => "Scientist",
+	 	"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
  		),
  	2 => array(
-	 	name => "Hermes",
-	 	setting => "Cam/Anarch",
-	 	clan => "Brujah",
-	 	archetype => "Troublemaker",
-	 	description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
+	 	"name" => "Hermes",
+	 	"setting" => "Cam/Anarch",
+	 	"clan" => "Brujah",
+	 	"archetype" => "Troublemaker",
+	 	"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
  		),
  	 	3 => array(
-	 	name => "Antonin",
-	 	setting => "Cam/Anarch",
-	 	clan => "Ventrue",
-	 	archetype => "Royalty",
-	 	description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
+	 	"name" => "Antonin",
+	 	"setting" => "Cam/Anarch",
+	 	"clan" => "Ventrue",
+	 	"archetype" => "Royalty",
+	 	"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae est cumque possimus excepturi aut eligendi quo deserunt iusto vitae ipsa nihil deleniti fugiat, accusantium aliquid fugit dolores sequi maxime quae, beatae eum corporis ad doloremque! Asperiores nihil illo corporis repudiandae perspiciatis animi quis, facere voluptates delectus rem. Culpa qui, ratione."
  		)
  	);
 
