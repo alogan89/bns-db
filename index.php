322086e7378cc11f082756712d4dedcab71c3777
Edward Seibert <taddewberries@gmail.com>
2016-03-14 19:57:39 -0400

    typo

--- index.php
+++ index.php
@@ -18,31 +18,31 @@
 include("inc/sample_characters.php");
 
 foreach ($characters as $character) {
-echo "<div class='pc_block'>".
-	"<a href='#''>".
-		"<div class='pc_img'>".
-				"<img src='img/character_placeholder.png' alt='Character Name Here'>".
-			"</div>".
-			"<div class='pc_block_info'>".
-				"<h1>".$character['name']."</h1>".
-	"</a>".
-		"<ul>".
-			"<li>".
-				"<h2>Setting:</h2>".
-				"<span>".$character["setting"]."</span>".
-			"</li>".
-			"<li>".
-				"<h2>Clan:</h2>".
-				"<span>".$character['clan']."</span>".
-			"</li>".
-			"<li>".
-				"<h2>Archetype:</h2>".
-				"<span>".$character['archetype']."</span>".
-			"</li>".
-		"</ul>".
-		"<p class='description'>".$character['description']."</p>".
-	"</div>".
-"</div>";
+	echo "<div class='pc_block'>".
+		"<a href='#''>".
+			"<div class='pc_img'>".
+					"<img src='img/character_placeholder.png' alt='Character Name Here'>".
+				"</div>".
+				"<div class='pc_block_info'>".
+					"<h1>".$character['name']."</h1>".
+		"</a>".
+			"<ul>".
+				"<li>".
+					"<h2>Setting:</h2>".
+					"<span>".$character["setting"]."</span>".
+				"</li>".
+				"<li>".
+					"<h2>Clan:</h2>".
+					"<span>".$character['clan']."</span>".
+				"</li>".
+				"<li>".
+					"<h2>Archetype:</h2>".
+					"<span>".$character['archetype']."</span>".
+				"</li>".
+			"</ul>".
+			"<p class='description'>".$character['description']."</p>".
+		"</div>".
+	"</div>";
 }
 ?>
 <!-- END LOOP -->
